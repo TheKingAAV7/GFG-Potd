69f976e52959a617ca8d7e9e1464d07449165500
Sushantbhoite7 <sushantbhoite27@gmail.com>
2024-09-20 00:32:15 +0530

    20Sept2024

---
+++ 20Sept2024.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    // Returns count buildings that can see sunlight
+    int countBuildings(vector<int> &height) {
+        int cnt=1;
+        int maxi=height[0];
+        for(int i=1;i<height.size();i++){
+            if(height[i]>maxi) cnt++;
+            maxi=max(maxi, height[i]);
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> height;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            height.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.countBuildings(height);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
