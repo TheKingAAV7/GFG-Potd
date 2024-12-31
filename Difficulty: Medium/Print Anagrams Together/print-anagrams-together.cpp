f06dbe4c9cf63143c4c3d7418f1a30db3aaa3154
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2024-12-31 11:18:47 -0800

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Print Anagrams Together/print-anagrams-together.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<string>> anagrams(vector<string>& arr) {
+        vector<vector<string>>ans;
+        unordered_map<string,int>mp;
+        int pos=0;
+        for(string s:arr){
+            string tmp=s;
+            sort(tmp.begin(),tmp.end());
+            if(mp.find(tmp)!=mp.end()){
+                ans[mp[tmp]].push_back(s);
+            }
+            else{
+                mp[tmp]=pos++;
+                ans.push_back({s});
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<string> arr;
+        string input_line;
+        getline(cin, input_line);
+        stringstream ss(input_line);
+        string word;
+        while (ss >> word) {
+            arr.push_back(word);
+        }
+
+        Solution ob;
+        vector<vector<string> > result = ob.anagrams(arr);
+        sort(result.begin(), result.end());
+        for (int i = 0; i < result.size(); i++) {
+            for (int j = 0; j < result[i].size(); j++) {
+                cout << result[i][j] << " ";
+            }
+            cout << "\n";
+        }
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
