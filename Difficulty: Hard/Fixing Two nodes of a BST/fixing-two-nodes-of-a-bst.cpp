f676435736579a84c97286105654c01af4ce37a7
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-02-14 04:41:46 -0800

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Fixing Two nodes of a BST/fixing-two-nodes-of-a-bst.cpp
@@ -0,0 +1,219 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node *left, *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+bool isBST(Node* n, int lower, int upper) {
+    if (!n)
+        return true;
+    if (n->data <= lower || n->data >= upper)
+        return false;
+    return (isBST(n->left, lower, n->data) && isBST(n->right, n->data, upper));
+}
+
+bool compare(Node* a, Node* b, vector<pair<int, int>>& mismatch) {
+    if (!a && !b)
+        return true;
+    if (!a || !b)
+        return false;
+
+    if (a->data != b->data)
+        mismatch.push_back(pair<int, int>(a->data, b->data));
+
+    return (compare(a->left, b->left, mismatch) &&
+            compare(a->right, b->right, mismatch));
+}
+
+
+// } Driver Code Ends
+/*
+class Node
+{
+    public:
+    int data;
+    Node *left, *right;
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+    
+  private:
+  
+  void maxi(Node* &ans,Node* root){
+      if(!root) return;
+      if(root->data>ans->data){
+          ans=root;
+      }
+      maxi(ans,root->left);
+      maxi(ans,root->right);
+      
+      return;
+  }
+  void mini(Node* &ans,Node* root){
+      if(!root) return;
+      if(root->data< ans->data){
+          ans=root;
+      }
+       mini(ans,root->left);
+      mini(ans,root->right);
+      
+  }
+  void f(Node* root){
+      if(!root) return;
+      Node * max_lef= new Node(INT_MIN);
+      maxi(max_lef,root->left);
+      Node * min_right= new Node(INT_MAX);
+      mini(min_right,root->right);
+      Node* cur=root;
+      if(max_lef->data < cur->data and cur->data > min_right->data){
+          swap(cur->data,min_right->data);
+          return;
+      }
+      else if(max_lef->data > cur->data and cur->data > min_right->data){
+          swap(max_lef->data,min_right->data);
+          return;
+      }
+      else if(max_lef->data > cur->data and min_right->data > cur->data){
+          swap(cur->data,max_lef->data);
+          return;
+      }
+      else{
+          f(root->left);
+          f(root->right);
+      }
+  }
+  public:
+    void correctBST(Node* root) {
+       f(root);
+       return;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    getchar();
+
+    while (t--) {
+        string s;
+        getline(cin, s);
+
+        Node* root = buildTree(s);
+        Node* duplicate = buildTree(s);
+
+        Solution ob;
+        ob.correctBST(root);
+
+        // check 1: is tree now a BST
+        if (!isBST(root, INT_MIN, INT_MAX)) {
+            cout << "0\n";
+            continue;
+        }
+
+        // check 2: comparing with duplicate tree
+
+        vector<pair<int, int>> mismatch;
+        // a vector to store data of mismatching nodes
+
+        if (!compare(root, duplicate, mismatch)) {
+            // false output from this function indicates change in structure of tree
+            cout << "0\n";
+            continue;
+        }
+
+        // finally, analysing the mismatching nodes
+        if (mismatch.size() != 2 || mismatch[0].first != mismatch[1].second ||
+            mismatch[0].second != mismatch[1].first)
+            cout << "0\n";
+        else
+            cout << "1\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
