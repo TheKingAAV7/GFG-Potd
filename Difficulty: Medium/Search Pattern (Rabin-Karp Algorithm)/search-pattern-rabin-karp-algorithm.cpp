f42fef3c8ca63ada299bf0a6cac1129119a07104
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-06-13 19:14:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.cpp
+++ Difficulty: Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.cpp
@@ -1,35 +1,46 @@
-#define ll long long
 class Solution {
+  private:
+  int binpowmod(int a, int b, int m) {
+    a %= m;
+    int res = 1;
+    while (b > 0) {
+        if (b & 1)
+    res = (int)((1LL * res * a) % m);
+a = (int)((1LL * a * a) % m);
+        b >>= 1;
+    }
+    return res;
+}
   public:
+  int mod=1e9+7;
     vector<int> search(string &pat, string &txt) {
-        int n = pat.length();
-        int m = txt.length();
-        vector<int> ans;
-        ll p = 31;
-        int mod = 1e9 + 7;
-        auto modexp = [&](ll b, ll e) {
-            ll r = 1;
-            while (e) {
-                if (e & 1) r = (r * b) % mod;
-                b = (b * b) % mod;
-                e >>= 1;
-            }
-            return r;
-        };
-        ll invP = modexp(p, mod - 2);
-        ll hash = 0, hash1 = 0, po = 1;
-        for (int i = 0; i < n; i++) {
-            hash = (hash + (pat[i] - 'a' + 1) * po % mod) % mod;
-            hash1 = (hash1 + (txt[i] - 'a' + 1) * po % mod) % mod;
-            if (i != n - 1) po = (po * p) % mod;
-        }
-        if (hash == hash1) ans.push_back(1);
-        for (int i = n; i < m; i++) {
-            hash1 = (hash1 - (txt[i - n] - 'a' + 1) + mod) % mod;
-            hash1 = (hash1 * invP) % mod;
-            hash1 = (hash1 + (txt[i] - 'a' + 1) * po % mod) % mod;
-            if (hash1 == hash) ans.push_back(i - n + 2);
-        }
-        return ans;
+       int n=pat.length();
+       int m=txt.length();
+       int p=31;
+       
+       vector<int>ans;
+       long long hsh=0;
+       long long po=1;
+     
+       long long hsh1=0;
+       for(int i=0;i<n;i++){
+           hsh=(hsh%mod+((long long)(pat[i]-'a'+1)*po)%mod)%mod;
+           hsh1=(hsh1%mod+((long long)(txt[i]-'a'+1)*po)%mod)%mod;
+           po=(po%mod*p%mod)%mod;
+       }
+       if(hsh==hsh1) ans.push_back(1);
+       
+       
+      
+       int invo=binpowmod(p,mod-2,mod);
+       po=(po%mod*invo%mod)%mod;
+       for(int i=n;i<m;i++){
+          
+           hsh1=(hsh1-(long long)(txt[i-n]-'a'+1)+mod)%mod;
+            hsh1=(hsh1%mod*invo%mod)%mod;
+           hsh1=(hsh1%mod+((long long)(txt[i]-'a'+1)*po)%mod)%mod;
+           if(hsh==hsh1) ans.push_back(i-n+2);
+       }
+       return ans;
     }
 };
