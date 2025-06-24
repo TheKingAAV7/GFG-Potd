fc82d497c1873e7ab820519c630abc2c92556f9d
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-06-24 13:01:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sum of Query II/sum-of-query-ii.cpp
+++ Difficulty: Medium/Sum of Query II/sum-of-query-ii.cpp
@@ -3,49 +3,43 @@
 class Solution {
   private:
   vector<int>segtree;
-  int bt(int i, int lef, int rig, int arr[],int n){
+  vector<int>nums;
+  
+  void bt(int i, int lef , int rig){
       if(lef==rig){
-        segtree[i]=arr[lef];
-        return segtree[i];
+          segtree[i]=nums[lef];
+          return;
       }
       int mid=(lef+rig)/2;
-      int left=bt(2*i+1,lef,mid,arr,n);
-      int right=bt(2*i+2,mid+1,rig,arr,n);
-      segtree[i]=left+right;
-      return segtree[i];
+      bt(2*i+1,lef, mid);
+      bt(2*i+2, mid+1,rig);
+      segtree[i]=segtree[2*i+1]+segtree[2*i+2];
+      return ;
   }
-  int sm(int i, int lef, int rig, int left, int right){
-      if(lef==left and right==rig){
-          return segtree[i];
-      }
+  int q(int i, int lef, int rig, int l, int r){
+      if(lef==l and rig==r) return segtree[i];
       int mid=(lef+rig)/2;
-      if(right<=mid){
-          return sm(2*i+1,lef,mid,left,right);
-      }
-      else if((mid+1)<=left ){
-          return sm(2*i+2,mid+1,rig,left,right);
-      }
-      int le=sm(2*i+1,lef,mid,left,mid);
-      int ri=sm(2*i+2,mid+1,rig,mid+1,right);
-      return le+ri;
+      if(r<=mid) return q(2*i+1,lef, mid, l, r);
+      else if((mid+1)<=l) return q(2*i+2,mid+1, rig, l ,r);
+      int left=q(2*i+1,lef,mid,l, mid);
+      int right=q(2*i+2,mid+1,rig, mid+1, r);
+      return left+right;
   }
+  
   public:
-    vector<int> querySum(int n, int arr[], int q, int queries[]) {
-     segtree.resize(4*n,0);
-     vector<int>ans;
-     bt(0,0,n-1,arr,n);
-     for(int i=0;i<2*q;i+=2){
-        //  cout<<queries[i]<<" "<<queries[i+1]<<endl;
-         int l=queries[i]-1;
-         int r=queries[i+1]-1;
-          
-         int tmp=sm(0,0,n-1,l,r);
-         ans.push_back(tmp);
-         
-         
-         
-     }
-     return ans;
-        
+    vector<int> querySum(int n, int arr[], int Q, int queries[]) {
+       
+        nums.resize(n,0);
+        vector<int>ans;
+        for(int i=0;i<n;i++) nums[i]=arr[i];
+        segtree.resize(4*n,0);
+        bt(0,0,n-1);
+        for(int i=0;i<2*Q;i+=2){
+            int l=queries[i]-1;
+            int r=queries[i+1]-1;
+            int tmp=q(0,0,n-1,l,r);
+            ans.push_back(tmp);
+        }
+        return ans;
     }
 };
