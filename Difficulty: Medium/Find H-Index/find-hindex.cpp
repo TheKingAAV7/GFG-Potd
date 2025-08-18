bf1eee1f2031e55a060d507698b91d2eb6d4c90a
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-08-18 08:16:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.cpp
+++ Difficulty: Medium/Find H-Index/find-hindex.cpp
@@ -1,65 +1,26 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
 class Solution {
   public:
-    // Function to find hIndex
-    int hIndex(vector<int>& cit) {
-        int n=cit.size();
-        int ans=0;
-       int low=0, high=n;
-       auto f=[&](int k)->bool{
-           int cnt=0;
-           for(int i: cit){
-               if(i>=k) cnt++;
-           }
-           return cnt>=k;
-       };
-       while(low<=high){
-           int mid=(low+high)/2;
-           if(f(mid)){
-               ans=mid;
-               low=mid+1;
-           }
-           else high=mid-1;
-       }
-       return ans;
-       
+    int hIndex(vector<int>& ct) {
+        // code here
+        sort(ct.begin(),ct.end());
+        
+        int n=ct.size();
+        int maxi=-1;
+        int lo=0,hi=ct.back();
+        while(lo<=hi){
+            int mid=(lo+hi)>>1;
+            auto it=lower_bound(ct.begin(),ct.end(),mid);
+            if(it!=ct.end()){
+            int idx=it-ct.begin();
+            int cnt=n-idx;
+            if(cnt>=mid){
+                maxi=mid;
+                lo=mid+1;
+            }
+            else hi=mid-1;
+            }
+            else hi=mid-1;
+        }
+        return maxi;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int test_cases;
-    cin >> test_cases;
-    cin.ignore();
-    while (test_cases--) {
-        string input;
-        vector<int> arr;
-
-        // Read the array from input line
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        // Solution instance to invoke the function
-        Solution ob;
-        int result = ob.hIndex(arr);
-
-        cout << result << endl;
-
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
