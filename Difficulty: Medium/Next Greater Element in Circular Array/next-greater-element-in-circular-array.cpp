fa668175ce764e06b78c446423ca0506629ef1e7
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-09-18 15:06:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.cpp
+++ Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.cpp
@@ -1,20 +1,22 @@
 class Solution {
   public:
-    vector<int> nextLargerElement(vector<int> &arr) {
-    int n=arr.size();
-    for(int i=0;i<n;i++) arr.push_back(arr[i]);
-    vector<int>ans(n,-1);
-    stack<int>st;
-    for(int i=0;i<2*n;i++){
+    vector<int> nextGreater(vector<int> &arr) {
+        // code here
+        // 9 7 3 1 4 2 10 9 8  9 7 3 1 4 2 10 9 8
+        int n=arr.size();
+        vector<int>ans(n,-1);
+        for(int i=0;i<n;i++) arr.push_back(arr[i]);
         
-        while(!st.empty() and arr[i]>arr[st.top()]){
-            if(st.top()<n) ans[st.top()]=arr[i];
-            st.pop();
+        //cout<<arr.size()<<endl;
+        stack<int>st;
+        for(int i=0;i<2*n;i++){
+            while(!st.empty() and arr[i]>arr[st.top()]){
+                int pos=st.top();st.pop();
+                if(pos<n)
+                ans[pos]=arr[i];
+            }
+            st.push(i);
         }
-        st.push(i);
-        
-    }
-    return ans;
-        
+        return ans;
     }
 };
