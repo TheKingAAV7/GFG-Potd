aff60c45650dae2fc88a50f74ad6c7b39962dd8b
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-10-27 11:07:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,41 +1,39 @@
 class Solution {
   public:
-  vector<int>rank;
-  vector<int>par;
-
-  int findpar(int node){
-      if(par[node]==node) return node;
-      return par[node]=findpar(par[node]);
-  }
-  
-  bool uni(int x, int y){
-      int parx=findpar(x);
-      int pary=findpar(y);
-      if(parx==pary) return true;
-      if(rank[parx]>rank[pary]){
-          par[pary]=parx;
-      }
-      else if(rank[pary]>rank[parx]){
-          par[parx]=pary;
-      }
-      else{
-          par[pary]=parx;
-          rank[parx]++;
-      }
-      return false;
-  }
+   vector<int>par,rank;
+   
+    int findpar(int node){
+        if(par[node]==node) return node;
+        return par[node]=findpar(par[node]);
+    }
+    bool uni(int x, int y){
+        int parx=findpar(x);
+        int pary=findpar(y);
+        if(parx==pary) return true;
+        if(rank[parx]>rank[pary]){
+            par[pary]=parx;
+        }
+        else if( rank[pary]>rank[parx]){
+            par[parx]=pary;
+        }
+        else{
+            par[pary]=parx;
+            rank[parx]++;
+        }
+        return false;
+    }
     bool isCycle(int n, vector<vector<int>>& edges) {
-        rank.resize(n,0);
+        // Code here
         par.resize(n);
-        
+        rank.resize(n);
         iota(par.begin(),par.end(),0);
+        
         for(auto it:edges){
-            int x=it[0],y=it[1];
+            int x=it[0];
+            int y=it[1];
             if(uni(x,y)) return true;
         }
         return false;
-        
-        
         
     }
 };
