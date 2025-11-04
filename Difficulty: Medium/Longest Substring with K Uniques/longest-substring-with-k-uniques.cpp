5cb4433ed6dd702d65bb6b00c16f6825dc577c27
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-11-04 12:29:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
@@ -1,26 +1,20 @@
 class Solution {
-  private:
-  int f(string s, int k){
-      map<char,int>mp;
-      int n=s.length();
-      int l=0;
-      int cnt=-1;
-      for(int i=0;i<n;i++){
-          mp[s[i]]++;
-          while(mp.size()>k){
-              mp[s[l]]--;
-              if(mp[s[l]]==0) mp.erase(s[l]);
-              l++;
-          }
-          if(mp.size()==k)
-          cnt= max(cnt,(i-l+1));
-      }
-      return cnt;
-  }
   public:
     int longestKSubstr(string &s, int k) {
-       
-       return f(s,k);
-        
+        // code here
+        int n= s.length();
+        int ans=-1;
+        map<char,int>mp;
+        int l=0;
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
+            while(mp.size()>k){
+                mp[s[l]]--;
+                if(mp[s[l]]==0) mp.erase(s[l]);
+                l++;
+            }
+            if(mp.size()==k) ans= max(ans, i-l+1);
+        }
+        return ans;
     }
 };
