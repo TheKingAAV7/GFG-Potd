70aa80f44dfb5b2fb9e1c621636ad05a4066efc7
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-12-16 01:30:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Strings Rotations of Each Other/strings-rotations-of-each-other.py
+++ Difficulty: Medium/Strings Rotations of Each Other/strings-rotations-of-each-other.py
@@ -1,29 +1,60 @@
 class Solution:
     def areRotations(self, s1, s2):
-        # code here
-        s1=s1+s1
-        res= s2 + '#' + s1
-        n= len(res)
-        z=[0]*n
-        l=0
-        r=-1
+        
+        # 2*31*31    1      
+        res= s1+s1
+        n=len(res)
         m=len(s2)
-        for i in range(1,n):
+        hshval=0
+        p=int(31)
+        mod=int(1e9+7)
+        po=int(0)
+        for i in range(m):
+            cur= ord(s2[i])-ord('a')+1
+            rig=(pow(p,po,mod)*cur)%mod
             
-            if i<=r:
-                z[i]=min(r-i+1,z[i-l])
-            while i+z[i]<n and res[z[i]]==res[i+z[i]]:
-                z[i]+=1
+            hshval= (hshval + rig)%mod
+            po= (po+1)%mod
+             
+        
+        #print(res,hshval,s2)
+        hsh=[0]*n
+        h1=0
+        po=int(0)
+        for i in range(n):
+            cur=ord(res[i])-ord('a')+1
+            rig=(pow(p,po,mod)*cur)%mod
+            h1=(h1+rig)%mod
+            hsh[i]=h1
+            po= (po+1)%mod
+        
+        
+        #print(hsh)
             
-            if (i+z[i]-1) > r:
-                r=i+z[i]-1
-                l=i
+        
+        l=int(0)
+        i=int(0)
+        while i<n:
+            while i-l+1 > m:
+                l+=1
                 
-                    
-        
-        
-        for i in range(m+1,n):
-            if z[i]==m:
-                return True
-        
+            
+            if i-l+1==m:
+                total= hsh[i]
+                lef=0
+                if l-1>=0:
+                    lef=hsh[l-1]
+                curhsh=(total-lef+mod)%mod
+                curbase=pow(p,l,mod)
+                inv= pow(curbase,mod-2,mod)
+                curhsh=(curhsh*inv)%mod
+                if curhsh == hshval:
+                    return True;
+                
+            i+=1
+            
+            
+                
+            
+            
         return False
