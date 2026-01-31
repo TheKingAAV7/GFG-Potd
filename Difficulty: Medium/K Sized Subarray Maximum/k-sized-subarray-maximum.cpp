d4a378fac1f79406b29e1b5e27c973de5037f358
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-02-01 01:12:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -3,19 +3,32 @@ class Solution {
     vector<int> maxOfSubarrays(vector<int>& arr, int k) {
         // code here
         int n=arr.size();
-        vector<int>ans;
-        map<int,int>mp;
-        for(int i=0;i<k;i++){
-            mp[arr[i]]++;
+        int K=25;
+        vector<vector<int>>dp(n,vector<int>(K,-1));
+        for(int i=0;i<n;i++) dp[i][0]=arr[i];
+        for(int b=1;b<25;b++){
+            for(int i=0;(i+(1<<b))<=n;i++){
+                dp[i][b]= max(dp[i][b-1],dp[i+(1<<(b-1))][b-1]);
+            }
         }
-        ans.push_back(mp.rbegin()->first);
-        for(int i=k;i<n;i++){
-            mp[arr[i-k]]--;
-            if(mp[arr[i-k]]==0) mp.erase(arr[i-k]);
-            mp[arr[i]]++;
-            ans.push_back(mp.rbegin()->first);
+        int MAXN=n;
+        int lg[MAXN+1];
+        lg[1] = 0;
+        for (int i = 2; i <= MAXN; i++)
+            lg[i] = lg[i/2] + 1;
+        vector<int>ans;
+        
+        auto f=[&](int L, int R)->int{
+            int  i = lg[R - L + 1];
+            int mx = max(dp[L][i], dp[R-(1<<i)+1][i]);
+            return mx;
+        };
+        for(int i=0;i+k-1<n;i++){
+            int tmp= f(i,i+k-1);
+            ans.push_back(tmp);
         }
         return ans;
+        
         
     }
 };
