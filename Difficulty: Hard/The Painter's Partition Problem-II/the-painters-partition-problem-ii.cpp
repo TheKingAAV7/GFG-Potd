2fca9cc22371bae795f321978ab62d26c1dec65c
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-02-14 00:05:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
@@ -2,36 +2,30 @@ class Solution {
   public:
     int minTime(vector<int>& arr, int k) {
         // code here
-        
         int n=arr.size();
-        
-        int lo = 1;
-        int hi = accumulate(arr.begin(),arr.end(),0ll);
-        
-        auto f= [&](int mid)->bool{
-            int cnt=1;
-            int cur=0;
-            for(int i=0;i<n;i++){
-                if(arr[i]>mid) return false;
-                if(cur+arr[i]>mid){
-                    cnt++;
-                    cur=arr[i];
-                }
-                else cur+=arr[i];
-            }
-            
-            
-            return cnt<=k;
+        int sm=accumulate(arr.begin(),arr.end(),0);
+        int lo=1, hi= sm;
+        int ans=hi;
+        auto f=[&](int mid)->bool{
+          int cnt=1;
+          int cursm=0;
+          for(int i=0;i<n;i++){
+              if(arr[i]>mid) return false;
+              if(cursm+arr[i]<=mid) cursm+=arr[i];
+              else {
+                  cnt++;
+                  cursm=arr[i];
+              }
+          }
+          return cnt<=k;
         };
-        
-        int ans= hi;        
         while(lo<=hi){
-        int mid=(lo+hi)>>1;
-        if(f(mid)){
-            ans=mid;
-            hi=mid-1;
-        }
-        else lo=mid+1;
+            int mid=(lo+hi)>>1;
+            if(f(mid)){
+                ans=mid;
+                hi=mid-1;
+            }
+            else lo=mid+1;
         }
         return ans;
         
