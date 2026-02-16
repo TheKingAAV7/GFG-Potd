ca95c5cc8382810130af7625adefc95c582a5309
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-02-16 16:33:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Meeting Rooms/meeting-rooms.cpp
+++ Difficulty: Easy/Meeting Rooms/meeting-rooms.cpp
@@ -2,17 +2,27 @@ class Solution {
   public:
     bool canAttend(vector<vector<int>> &arr) {
         // Code Here
-        sort(arr.begin(),arr.end());
-        int l=-1,r=-1;
-        for(auto it:arr){
-            int start= it[0];
-            int end  = it[1];
-            if(start>=r){
-                l=start;
-                r=end;
-            }
-            else return false;
+    int n = arr.size();
+    sort (arr.begin(), arr.end());
+    /*
+ (1,4) (7,10) (10,15)
+ (2,4)  (6,10) (9,12) 
+ 
+ 
+  1 4
+  10 15
+  7 10
+    
+    */
+    for (int i=0; i<n-1; i++) {
+        if ( arr[i][1] > arr[i+1][0] ) {
+            return false;
         }
-        return true;
+        
+    }
+    return true;
+    
+    
+    
     }
 };
