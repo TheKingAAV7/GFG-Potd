1ca2cad96b19034b8e63ecb7fc118194431d4acf
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-02-18 18:49:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.cpp
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -1,49 +1,54 @@
-
-class FenwickTree{
-  public:
-  vector<int>bit;
-  int n;
-  FenwickTree(int n){
-      this->n=n;
-      bit.resize(n,0);
-      // t(i) = answer for [g(i),i]
-        //                    [g(g(i-1)),g(i-1)]
-  }
-  void increase(int idx, int val){
-      for(;idx<n;idx= idx|(idx+1)){
-          bit[idx]+=val;
-      }
-      
-  }
-  int sum(int r){
-      int sm=0;
-      while(r>=0){
-          sm+=bit[r];
-          r=(r&(r+1))-1;
-      }
-      return sm;
-  }
-  
-  int query(int l, int r){
-      return sum(r)-sum(l-1);
-  }
+class SegmentTree{
+    public:
+    int n;
+    vector<int>st;
+    SegmentTree(int n){
+        this->n=n;
+        st.resize(4*n,0);
+    }
+    
+    void up(int idx, int lef, int rig, int i, int val){
+        if(lef>rig) return ;
+        if(lef==rig){
+            st[idx]+=val;
+            return;
+        }
+        int mid=(lef+rig)>>1;
+        if(i<=mid) up(2*idx+1,lef,mid,i,val);
+        else up(2*idx+2,mid+1,rig,i,val);
+        st[idx]=st[2*idx+1]+st[2*idx+2];
+        return;
+    }
+    
+    int query(int idx, int lef, int rig, int left, int right){
+        if(lef>rig || left>right) return 0;
+        if(lef==left and rig==right) return st[idx];
+        int mid=(lef+rig)>>1;
+        if(right<=mid)  return query(2*idx+1,lef,mid,left,right);
+        if((mid+1)<=left) return query(2*idx+2,mid+1,rig,left,right);
+        int lefsm= query(2*idx+1,lef,mid,left,mid);
+        int rigsm= query(2*idx+2,mid+1,rig,mid+1,right);
+        return lefsm+rigsm;
+    }
 };
+
 class Solution {
   public:
     int inversionCount(vector<int> &arr) {
         // Code Here
         int n=arr.size();
-        int MAXI=1e4+1;
-        FenwickTree ft= FenwickTree(MAXI);
-        // count the bigger on the left
-        
+        int MAXI= 1e4+1;
+        SegmentTree tree= SegmentTree(MAXI);
+        // find all greater
         int ans=0;
         for(int i=0;i<n;i++){
-            int cnt= ft.query(arr[i]+1,1e4);
+            int cnt=0;
+            cnt=tree.query(0,0,MAXI,arr[i]+1,1e4); 
             ans+=cnt;
-            ft.increase(arr[i],1);
-            
+            tree.up(0,0,MAXI,arr[i],1);
         }
         return ans;
+        
+        
     }
 };
