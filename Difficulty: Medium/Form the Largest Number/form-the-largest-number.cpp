9ef18db57830ecc6030c4d389987b22a84b20afc
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-02-20 00:22:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form the Largest Number/form-the-largest-number.cpp
+++ Difficulty: Medium/Form the Largest Number/form-the-largest-number.cpp
@@ -1,47 +1,18 @@
-class cmp {
-  public:
-    bool operator()(string s1, string s2) {
-        return s1 + s2 > s2 + s1;
-    }
-};
 class Solution {
   public:
     string findLargest(vector<int> &arr) {
         // code here
-        int n = arr.size();
-        vector<string> v(n);
         
-        string num = "";
-        int dig;
-        char digit;
-        int x;
-        
-        for(int i = 0; i < n; i++) {
-            x = arr[i];
-            num = "";
-            
-            while(x) {
-                dig = x % 10;
-                digit = dig + '0';
-                num = digit + num;
-                x /= 10;
-            }
-            
-            if(num == "") v[i] = "0";
-            else v[i] = num;
-        }
-        
-        sort(v.begin(), v.end(), cmp());
-        
-        string ans = "";
-        
-        int f = 1; // all are zero
-        for(string &s: v) {
-            if(s != "0") f = 0;
-            ans += s;
-        }
-        if(f) return "0";
-        
+        sort(arr.begin(),arr.end(),[&](int a, int b){
+           string s1= to_string(a);
+           string s2= to_string(b);
+           string a1= s1+s2;
+           string a2= s2+s1;
+           return a1>=a2;
+        });
+        string ans;
+        for(int i:arr) ans+=to_string(i);
+        if(arr[0]==0) return "0";
         return ans;
         
     }
