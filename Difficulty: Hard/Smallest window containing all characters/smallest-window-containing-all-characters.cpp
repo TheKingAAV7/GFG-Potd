806b5234274251494108ad134b0e6cde7fa13dad
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-03-06 00:23:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.cpp
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.cpp
@@ -1,25 +1,43 @@
 class Solution {
   public:
-    string smallestWindow(string &s, string &p) {
-       int n=s.length();
-       vector<int>mp(26,0);
-       for(char c:p) mp[c-'a']++;
-       int sz=INT_MAX;
-       int lef=-1,r=-1;
-       int l=0;
-       for(int i=0;i<n;i++){
-           mp[s[i]-'a']--;
-           while(*max_element(mp.begin(),mp.end())==0){
-               if(i-l+1<sz){
-                   sz=i-l+1;
-                   lef=l;
-                   r=i;
-               }
-               mp[s[l]-'a']++;
-               l++;
-           }
-       }
-       if(sz==INT_MAX) return "";
-       return s.substr(lef,sz);
+    string minWindow(string &s, string &p) {
+        // code here
+        int n=s.length();
+        int m=p.length();
+        vector<int>mp(26,-1e7);
+        for(int i=0;i<m;i++){
+            if(mp[p[i]-'a']==-1e7){
+                mp[p[i]-'a']=1;
+            }
+            else
+            mp[p[i]-'a']++;
+        }
+        
+        int al,ar;
+        al=ar=-1;
+        int l=0;
+        int len=INT_MAX;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(mp[s[i]-'a']!=-1e7)
+            mp[s[i]-'a']--;
+            while(*max_element(mp.begin(),mp.end())<=0){
+                 int curlen= i-l+1;
+                 if(curlen<len){
+                     al=l;
+                     ar=i;
+                     len=curlen;
+                 }
+                 
+                if(mp[s[l]]!=-1e7){
+                    mp[s[l]-'a']++;
+                }
+                l++;
+            }
+            
+        }
+        if(al==-1) return "";
+        return s.substr(al,len);
+        
     }
 };
