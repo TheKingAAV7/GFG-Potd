c89b68de833a1b25cf3a58f40ae575cda471c276
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-03-09 00:11:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Largest number in one swap/largest-number-in-one-swap.cpp
+++ Difficulty: Easy/Largest number in one swap/largest-number-in-one-swap.cpp
@@ -2,27 +2,28 @@ class Solution {
   public:
     string largestSwap(string &s) {
         // code here
-        // 885446
+        /*
+        
+        replace the leftmost with the maximum
+        */
         int n=s.length();
-        vector<int>mx(n,-1);
-        char maxi='.';
-        int pos=-1;
+        vector<int>suf(n,n-1);
+        char maxi='0'-1;
+        int idx=n-1;
         for(int i=n-1;i>=0;i--){
-            mx[i]=pos;
-            if(maxi=='.' || s[i]>maxi){
+            if(s[i]>maxi){
                 maxi=s[i];
-                pos=i;
+                idx=i;
             }
-           
+            suf[i]=idx;
         }
-        // for(int c:mx) cout<<c<<" ";
-        // cout<<endl;
-        for(int i=0;i<n;i++){
-            if(mx[i]!=-1 and s[mx[i]]>s[i]) {
-                swap(s[mx[i]],s[i]);
-                break;
-            }
+        for(int i=0;i<n-1;i++){
+            if(s[i]<s[suf[i+1]]){
+                swap(s[i],s[suf[i+1]]);
+                return s;
+            } 
         }
         return s;
+        
     }
 };
