7cc478027f24a32b9e537ef3fdcd94929cff712c
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-03-26 00:24:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Ways to Arrive at Destination/number-of-ways-to-arrive-at-destination.cpp
+++ Difficulty: Medium/Number of Ways to Arrive at Destination/number-of-ways-to-arrive-at-destination.cpp
@@ -2,51 +2,43 @@ class Solution {
   public:
     int countPaths(int n, vector<vector<int>>& edges) {
         // code here
-        
         vector<vector<array<int,2>>>adj(n);
         for(auto it:edges){
             adj[it[0]].push_back({it[1],it[2]});
             adj[it[1]].push_back({it[0],it[2]});
         }
+        // cost,node
         
+        priority_queue<array<int,2>,vector<array<int,2>>,greater<array<int,2>>>pq;
+        pq.push({0,0});
         vector<int>dp(n,0);
         vector<int>dist(n,INT_MAX);
-        // cost, node
-        priority_queue<array<int,2>,vector<array<int,2>>,greater<array<int,2>>>pq;
-        
-        pq.push({0,0});
-        dp[0]=1;
         dist[0]=0;
-        
+        dp[0]=1;
         while(!pq.empty()){
-            auto cur= pq.top();
-            pq.pop();
-            int cst= cur[0];
-            int nd= cur[1];
-           if(cst>dist[nd]) continue;
-            
-            
-            for(auto it:adj[nd]){
-                int node= it[0];
-                int wt= it[1];
-                int ndist= cst + wt;
-                if(ndist<dist[node]){
-                    dist[node]= ndist;
-                    dp[node]=dp[nd];
-                    pq.push({ndist,node});
+            auto cur= pq.top();pq.pop();
+            int cost=cur[0];
+            int node= cur[1];
+            // cout<<"Node "<<node<<endl;
+            for(auto it:adj[node]){
+                int ncost= it[1]+cost;
+                if(ncost<dist[it[0]]){
+                    // cout<<it[0]<<" ";
+                    dist[it[0]]=ncost;
+                    dp[it[0]]=1;
+                    pq.push({ncost,it[0]});
                 }
-                else if(ndist==dist[node]){
-                    dp[node]+= dp[nd];
+                else if(ncost==dist[it[0]]){
+                    // cout<<it[0]<<" ";
+                    dp[it[0]]++;
+                    pq.push({ncost,it[0]});
                 }
             }
+            // cout<<endl;
         }
-        
-        // for(int i:dist) cout<<i<<" ";
+        // for(int i:dp) cout<<i<<" ";
         // cout<<endl;
+        return dp[n-1];
         
-        // for(int i=0;i<n;i++) cout<<dp[i]<<" ";
-        // cout<<endl;
-        
-        return  dp[n-1];
     }
 };
