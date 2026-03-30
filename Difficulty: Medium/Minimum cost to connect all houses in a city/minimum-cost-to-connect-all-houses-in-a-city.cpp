6d98eabcdcb418203f06b9fce2573b371df524e1
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-03-30 09:12:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum cost to connect all houses in a city/minimum-cost-to-connect-all-houses-in-a-city.cpp
+++ Difficulty: Medium/Minimum cost to connect all houses in a city/minimum-cost-to-connect-all-houses-in-a-city.cpp
@@ -1,102 +1,68 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-  private:
-  vector<int>rank,parent;
-  
-  int findpar(int u){
-      if(parent[u]==u) return u;
-      return parent[u]=findpar(parent[u]);
-  }
-  bool uni(int u,int v){
-      int upar=findpar(u);
-      int vpar=findpar(v);
-      if(upar==vpar) return false;
-      if(rank[upar]>rank[vpar]){
-          parent[vpar]=upar;
-      }
-      else if(rank[vpar]>rank[upar]){
-          parent[upar]=vpar;
-      }
-      else{
-          parent[vpar]=upar;
-          rank[upar]++;
-      }
-      return true;
-  }
   public:
+    vector<int>parent;
+    vector<int>rank;
+    
+    int findpar(int x){
+        if(parent[x]==x) return x;
+        return parent[x]=findpar(parent[x]);
+    }
+    
+    bool uni(int u, int v){
+        int paru=findpar(u);
+        int parv=findpar(v);
+        if(paru==parv) return false;
+        
+        if(rank[paru]>rank[parv]){
+            parent[parv]=paru;
+        }
+        else if(rank[parv]>rank[paru]){
+            parent[paru]=parv;
+        }
+        else{
+            parent[parv]=paru;
+            rank[paru]++;
+        }
+        
+        return true;
+    }
     int minCost(vector<vector<int>>& houses) {
-      int n=houses.size();
-      map<string,int>mp;
-      set<pair<int,pair<int,int>>>st;
-      
-      
-      int cnt=0;
-      for(int i=0;i<n;i++){
-          string tmp= to_string(houses[i][0])+'#'+to_string(houses[i][1]);
-          mp[tmp]=cnt++;
-      }
-      for(int i=0;i<n;i++){
-           string tmp= to_string(houses[i][0])+'#'+to_string(houses[i][1]);
-           for(int j=i+1;j<n;j++){
-                   string tmp1=to_string(houses[j][0])+'#'+to_string(houses[j][1]);
-                   int val=abs(houses[i][0]-houses[j][0]) + abs(houses[i][1]-houses[j][1]);
-                   st.insert({val,{mp[tmp],mp[tmp1]}}); 
-           }
-           
-      }
-      int ans=0;
-      rank.resize(n,0);
-      parent.resize(n,0);
-      iota(parent.begin(),parent.end(),0);
-      for(auto it:st){
-          
-         int x=it.second.first;
-         int y=it.second.second;
-        //  cout<<it.first<<" "<<it.
-         if(uni(x,y)){
-             ans+=it.first;
-         }
-      }
-      return ans;
-      
+        // code here
+        
+        map<array<int,2>,int>mp;
+        int n=houses.size();
+
+        int nod=0;
+        for(auto it:houses){
+            mp[{it[0],it[1]}]=nod++;
+        }
+        // cost,
+        priority_queue<array<int,3>,vector<array<int,3>>,greater<array<int,3>>>pq;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int x1=houses[i][0];
+                int y1=houses[i][1];
+                int x2=houses[j][0];
+                int y2=houses[j][1];
+                int dist= abs(x1-x2) + abs(y1-y2);
+                pq.push({dist,mp[{x1,y1}],mp[{x2,y2}]});
+            }
+        }
+        parent.resize(n,0);
+        rank.resize(n,0);
+        iota(parent.begin(),parent.end(),0);
+        int ans=0;
+        while(!pq.empty()){
+            auto cur=pq.top();pq.pop();
+            int cost= cur[0];
+            int nd1= cur[1];
+            int nd2= cur[2];
+            if(uni(nd1,nd2)){
+                ans+=cost;
+            }
+        }
+        return ans;
+        
         
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> edges;
-
-        for (int i = 0; i < n; ++i) {
-            vector<int> temp;
-            for (int j = 0; j < 2; ++j) {
-                int x;
-                cin >> x;
-                temp.push_back(x);
-            }
-            edges.push_back(temp);
-        }
-
-        Solution obj;
-
-        cout << obj.minCost(edges);
-        cout << "\n";
-        cout << "~" << endl;
-    }
-}
-
-// } Driver Code Ends
