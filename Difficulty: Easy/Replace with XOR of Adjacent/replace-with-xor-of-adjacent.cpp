98bd522b9f5aaa79436dcbbc61a0a393a73dd2e8
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-05-30 16:01:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Replace with XOR of Adjacent/replace-with-xor-of-adjacent.cpp
+++ Difficulty: Easy/Replace with XOR of Adjacent/replace-with-xor-of-adjacent.cpp
@@ -1,16 +1,29 @@
 class Solution {
   public:
+  
     void replaceElements(vector<int>& arr) {
         // code here
-        int n=arr.size();
-        vector<int>v(n);
-        v[n-1] = arr[n-2] ^ arr[n-1];
-        for(int i=1;i<n-1;i++){
-            v[i]=arr[i-1]^arr[i+1];
-        }
-        v[0] = arr[0] ^ arr[1];
-        arr=v;
-        return ;
+        int n = arr.size();
         
+        int last = arr[n-1]^arr[n-2];
+        
+        int sum = arr[0];
+            
+        arr[0] = arr[0]^arr[1];
+        
+        if(n==2)
+        {
+            arr[n-1]=last;
+            return;
+        }
+        
+        for(int i=1;i<n-1;i++)
+        {
+            int prev=arr[i];
+            arr[i]=sum^arr[i+1];
+            sum=prev;
+        }
+        
+        arr[n-1]=last;
     }
 };
