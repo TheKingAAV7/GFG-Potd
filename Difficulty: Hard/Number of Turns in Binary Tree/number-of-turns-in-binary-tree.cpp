e86347535191f6fb653048e12c568c1102beb417
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-08-22 19:08:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Number of Turns in Binary Tree/number-of-turns-in-binary-tree.cpp
+++ Difficulty: Hard/Number of Turns in Binary Tree/number-of-turns-in-binary-tree.cpp
@@ -1,164 +1,76 @@
-//{ Driver Code Starts
-//Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node
-{
+/* Structure of Binary Tree Node
+class Node {
+  public:
     int data;
-    struct Node *left;
-    struct Node *right;
+    Node *left;
+    Node *right;
 
     Node(int val) {
         data = val;
-        left = right = NULL;
+        left = right = nullptr;
     }
 };
 
-// Function to Build Tree
-Node* buildTree(string str)
-{   
-    // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for(string str; iss >> str; )
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node *root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-      
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-          
-        // Get the current node's value from the string
-        string currVal = ip[i];
-          
-        // If the left child is not null
-        if(currVal != "N") {
-            // Create the left child for the current Node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-          
-        // For the right child
-        i++;
-        if(i >= ip.size())
-            break;
-        currVal = ip[i];
-          
-        // If the right child is not null
-        if(currVal != "N") {
-            // Create the right child for the current node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
 
 
-// } Driver Code Ends
-//User function template for C++
-/*
-Structure of the node of the tree is as follows 
-struct Node {
-    struct Node* left, *right;
-    int data;
-};
+first find out a LCA and 
+
 */
 
-class Solution{
-  private:
-  void f(Node* root,int d,string &path,string tmp){
-      if(!root) return;
-      if(root->data==d){
-          path=tmp;
-          return;
-      }
-      f(root->left,d,path,tmp+'L');
-      f(root->right,d,path,tmp+'R');
-      return;
-  }
+class Solution {
   public:
-    // function should return the number of turns required to go from first node to second node 
-    int NumberOFTurns(struct Node* root, int first, int second)
-    {
-      string p1="",p2="";
-      f(root,first,p1,"");
-      f(root,second,p2,"");
-        int i=0,j=0;
-        int n=p1.length();
-        int m=p2.length();
-        while(i<n and j<m and p1[i]==p2[j]){
-            i++;
-            j++;
-        }
-        // cout<<p1<<endl;
-        // cout<<p2<<endl;
-        // cout<<i<<" "<<j<<endl;
-        int cnt1=0,cnt2=0;
-        if(i<=n-1){
-            cnt1=1;
-            for(int k=i+1;k<n;k++){
-                if(p1[k]!=p1[k-1]) cnt1++;
+    int numberOfTurns(Node* root, int p, int q) {
+        // code here
+        map<Node*,int>mp;
+        
+        Node* LCA=NULL;
+        auto f=[&](auto &&self, Node* root )->vector<int>{
+         if(!root) return {};
+         
+        
+         auto lef= self(self,root->left);
+         auto rig= self(self,root->right);
+         
+         
+         if(root->data==p) lef.push_back(p);
+         if(root->data==q) rig.push_back(q);
+         
+         set<int>st(lef.begin(),lef.end());
+         for(int i:rig) st.insert(i);
+         vector<int>v(st.begin(),st.end());
+         if((int)(v.size())==2 and !LCA){
+          LCA=root;
+         }
+         return v;
+        };
+        
+        f(f,root);
+        
+        int a, b;
+        a=b=0;
+        auto f1=[&](auto &&self, Node* root, int &val, int prev,int turns)->void{
+            if(!root) return;
+            if(root->data==val){
+                if(val==p) a=turns;
+                else b= turns;
+                return;
             }
-        }
-        if(j<=m-1){
-            cnt2=1;
-            for(int k=j+1;k<m;k++){
-                if(p2[k]!=p2[k-1]) cnt2++;
-            }
-        }
-      int ans=cnt1+cnt2-1;
-      return ans;
+            self(self,root->left,val,0,turns+(prev!=0));
+            self(self,root->right,val,1,turns+(prev!=1));
+            return;
+        };
+        
+        // cout<<LCA->data<<endl;
+        f1(f1,LCA,p,-1,0);
+        f1(f1,LCA,q,-1,0);
+        
+        // cout<<a<<" "<<b<<endl;
+        
+        int ans= a+b;
+        if(ans==1) return -1;
+        
+        
+        return ans-1;
+        
     }
 };
-
-//{ Driver Code Starts.
-
-int main()
-{
-    int t;
-    struct Node *child;
-    scanf("%d ", &t);
-    while (t--)
-    {
-        string s;
-        getline(cin,s);
-        Node* root = buildTree(s);
-        int turn, first, second;
-        cin>>first>>second;
-        Solution ob;
-        if ((turn = ob.NumberOFTurns(root, first, second) ))
-            cout<<turn<<endl;
-        else 
-            cout<<"-1\n";
-        cin.ignore();
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-
-// } Driver Code Ends
