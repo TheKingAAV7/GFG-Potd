2a51bbf37c1b01fccff092e97cd90fdddeefa564
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-09-19 22:20:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Cost To Make Two Strings Identical/min-cost-to-make-two-strings-identical.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int findMinCost(string &s1, string &s2, int c1, int c2) {
+        // code here
+        
+        int m=s1.size(), n=s2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for (int i = 1; i <= m; ++i)
+        {
+            for (int j = 1; j <= n; ++j)
+            {
+                if (s1[i - 1] == s2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return (m-dp[m][n])*c1+(n-dp[m][n])*c2;
+    }
+};
